e37dab2b4026bff2a6a100b002fe409916e1f9f9
Jakub Gryczka <gryczka.jakub@gmail.com>
2021-10-23 18:46:22 +0200

    fixed clustergroup, added icons for markers

--- script.js
+++ script.js
@@ -7,6 +7,27 @@ function flyToParking() {
     mymap.flyTo(parking1.getLatLng(), zoomlvl);
   });
 }
+function truckDetails() {
+  if (!vehicle1.reservationEnd || this.reservation) {
+    vehicle1.reservationEnd = 'Not reserved by anyone';
+    vehicle1.reservation = 'Not reserved';
+  }
+  if (!vehicle1.promotion) vehicle1.promotion = 'Excluded from promotion';
+  if (!vehicle1.description) vehicle1.description = 'No description';
+  if (!vehicle1.metadata) vehicle1.metadata = 'No metadata';
+}
+function carDetails() {
+  if (!vehicle2.reservationEnd || this.reservation) {
+    vehicle2.reservationEnd = 'Not reserved by anyone';
+    vehicle2.reservation = 'Not reserved';
+  }
+  if (!vehicle2.promotion) vehicle2.promotion = 'Excluded from promotion';
+  if (!vehicle2.description) vehicle2.description = 'No description';
+  if (!vehicle2.metadata) vehicle2.metadata = 'No metadata';
+  // if (!vehicle3.reservationEnd || this.reservation) {
+  //   vehicle3.reservationEnd = 'Not reserved by anyone';
+  //   vehicle3.reservation = 'Not reserved';
+}
 
 const vehicle1 = {
   platesNumber: 'WZPV001',
@@ -26,6 +47,7 @@ const vehicle1 = {
   metadata: null,
   isTruck: true,
 };
+truckDetails();
 const vehicle2 = {
   platesNumber: 'WZ8748W',
   sideNumber: 'Z1-WH-01',
@@ -40,6 +62,25 @@ const vehicle2 = {
   promotion: null,
   id: '00000000-0000-0000-0005-000000000001',
   name: 'Nissan Leaf White',
+  description: null,
+  metadata: null,
+  isCar: true,
+};
+carDetails();
+const vehicle3 = {
+  platesNumber: 'WZPC001',
+  sideNumber: 'Z4-PCAR-01',
+  color: 'white',
+  type: 'CAR',
+  rangeKm: 193,
+  batteryLevelPct: 98,
+  reservationEnd: null,
+  reservation: null,
+  status: 'AVAILABLE',
+  locationDescription: 'Vehicle is located on the first floor of the parking',
+  promotion: null,
+  id: '00000000-0000-0000-0005-000000000004',
+  name: 'Enigma Python Car',
   description: null,
   metadata: null,
   isCar: true,
@@ -76,6 +117,22 @@ const nissanCarBind = `<font size="4"><b>${vehicle2.name}</b></font>
 <br>Metadata of the vehicle: ${vehicle2.metadata}
 <br>ID of the vehicle: ${vehicle2.id}`;
 
+const enigmaCarBind = `<font size="4"><b>${vehicle3.name}</b></font>
+<br>Plates number: ${vehicle3.platesNumber}
+<br>Side number: ${vehicle3.sideNumber}
+<br>Vehicle color: ${vehicle3.color}
+<br>Type of vehicle: ${vehicle3.type}
+<br><b>Vehicle range in km: ${vehicle3.rangeKm}</b>
+<br><b>Battery level: ${vehicle3.batteryLevelPct}%</b>
+<br><b>Reservation ends in: ${vehicle3.reservationEnd}</b>
+<br><b>Vehicle is reserved by: ${vehicle3.reservation}</b>
+<br><b>Status of car: ${vehicle3.status}</b>
+<br><b>Location: ${vehicle3.locationDescription}</b>
+<br>Current promotions: ${vehicle3.promotion}
+<br>Description of the vehicle: ${vehicle3.description}
+<br>Metadata of the vehicle: ${vehicle3.metadata}
+<br>ID of the vehicle: ${vehicle3.id}`;
+
 const mymap = L.map('mapid').setView([52.19349790168748, 20.93058149735026], 13);
 
 const myLocation = L.control
@@ -105,7 +162,7 @@ const parking1 = L.circle([52.19339312958948, 20.930523991888947], {
   color: 'blue',
   fillColor: '#1F51FF',
   fillOpacity: 0.5,
-  radius: 30,
+  radius: 80,
 }).addTo(mymap);
 flyToParking();
 parking1.bindPopup(`<b><font size = "3">Parking for vehicles.</font></b>
@@ -114,62 +171,41 @@ parking1.bindPopup(`<b><font size = "3">Parking for vehicles.</font></b>
 //const truckMarker = L.marker([52.1935161702226, 20.9304286193486]).addTo(mymap);
 //const carMarker1 = L.marker([52.1935051400699, 20.930787718473074]).addTo(mymap);
 
-const truckDetails = function () {
-  if (!vehicle1.reservationEnd || this.reservation) {
-    vehicle1.reservationEnd = 'Not reserved by anyone';
-    vehicle1.reservation = 'Not reserved';
-  }
-  if (!vehicle1.promotion) vehicle1.promotion = 'Excluded from promotion';
-  if (!vehicle1.description) vehicle1.description = 'No description';
-  if (!vehicle1.metadata) vehicle1.metadata = 'No metadata';
-};
-truckDetails();
+const truckIcon = L.icon({
+  iconUrl: '/assets/a_truck.png',
+  iconSize: [35, 35],
+  iconAnchor: [20, 25],
+  popupAnchor: [-3, -15],
+});
+const carIcon = L.icon({
+  iconUrl: '/assets/a_car.png',
+  iconSize: [35, 35],
+  iconAnchor: [20, 25],
+  popupAnchor: [-3, -15],
+});
+
 const addressPoints = [
   [52.1935161702226, 20.9304286193486, vehicle1.name],
   [52.193275, 20.930372, vehicle2.name],
-  [52.1935051400688, 20.93078771847324, '3'],
+  [52.193891367697, 20.930564789789, vehicle3.name],
 ];
+
 const marker = [];
 const markers = L.markerClusterGroup();
 for (let i = 0; i < addressPoints.length; i++) {
   let a = addressPoints[i];
   let title = a[2];
-  marker[i] = 'siema';
   marker[i] = L.marker(new L.LatLng(a[0], a[1]), {
     title: title,
+    icon: marker[0] ? carIcon : truckIcon,
   });
   if (title === `${vehicle1.name}`) {
     marker[i].bindPopup(enigmaTruckBind);
   } else if (title === `${vehicle2.name}`) {
     marker[i].bindPopup(nissanCarBind);
+  } else {
+    marker[i].bindPopup(enigmaCarBind);
   }
   markers.addLayer(marker[i]);
 }
 mymap.addLayer(markers);
-// it's doing literally nothing
-// const vehicles = [vehicle1, vehicle2];
-// for (let i = 0; i < vehicles.length; i++) {
-//   let a = vehicles[i];
-//   let popup = `<font size="4"><b>${a.name}</b></font>
-//   <br>Plates number: ${a.platesNumber}
-//   <br>Side number: ${a.sideNumber}
-//   <br>Vehicle color: ${a.color}
-//   <br>Type of vehicle: ${a.type}
-//   <br><b>Vehicle range in km: ${a.rangeKm}</b>
-//   <br><b>Battery level: ${a.batteryLevelPct}%</b>
-//   <br><b>Reservation ends in: ${a.reservationEnd}</b>
-//   <br><b>Vehicle is reserved by: ${a.reservation}</b>
-//   <br><b>Status of car: ${a.status}</b>
-//   <br><b>Location: ${a.locationDescription}</b>
-//   <br>Current promotions: ${a.promotion}
-//   <br>Description of the vehicle: ${a.description}
-//   <br>Metadata of the vehicle: ${a.metadata}
-//   <br>ID of the vehicle: ${a.metadata}`;
-// }
-
-const truckIcon = L.icon({
-  iconUrl: 'images/a_truck.png',
-  iconSize: [38, 95],
-  iconAnchor: [22, 94],
-  popupAnchor: [-3, -76],
-});
