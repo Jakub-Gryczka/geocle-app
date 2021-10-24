ee863f5d6c2b455a47647a802617f55d379d3f55
Jakub Gryczka <gryczka.jakub@gmail.com>
2021-10-24 14:59:45 +0200

    made filtering, optimalized code, little fixes

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 'use strict';
-const clusterGroup = document.querySelector('.marker-cluster');
 const parkingButton = document.querySelector('.find-parking');
 function flyToParking() {
   L.DomEvent.on(parkingButton, 'click', function () {
@@ -7,28 +6,6 @@ function flyToParking() {
     mymap.flyTo(parking1.getLatLng(), zoomlvl);
   });
 }
-function truckDetails() {
-  if (!vehicle1.reservationEnd || this.reservation) {
-    vehicle1.reservationEnd = 'Not reserved by anyone';
-    vehicle1.reservation = 'Not reserved';
-  }
-  if (!vehicle1.promotion) vehicle1.promotion = 'Excluded from promotion';
-  if (!vehicle1.description) vehicle1.description = 'No description';
-  if (!vehicle1.metadata) vehicle1.metadata = 'No metadata';
-}
-function carDetails() {
-  if (!vehicle2.reservationEnd || this.reservation) {
-    vehicle2.reservationEnd = 'Not reserved by anyone';
-    vehicle2.reservation = 'Not reserved';
-  }
-  if (!vehicle2.promotion) vehicle2.promotion = 'Excluded from promotion';
-  if (!vehicle2.description) vehicle2.description = 'No description';
-  if (!vehicle2.metadata) vehicle2.metadata = 'No metadata';
-  // if (!vehicle3.reservationEnd || this.reservation) {
-  //   vehicle3.reservationEnd = 'Not reserved by anyone';
-  //   vehicle3.reservation = 'Not reserved';
-}
-
 const vehicle1 = {
   platesNumber: 'WZPV001',
   sideNumber: 'Z3-PVAN-01',
@@ -47,7 +24,6 @@ const vehicle1 = {
   metadata: null,
   isTruck: true,
 };
-truckDetails();
 const vehicle2 = {
   platesNumber: 'WZ8748W',
   sideNumber: 'Z1-WH-01',
@@ -66,7 +42,6 @@ const vehicle2 = {
   metadata: null,
   isCar: true,
 };
-carDetails();
 const vehicle3 = {
   platesNumber: 'WZPC001',
   sideNumber: 'Z4-PCAR-01',
@@ -85,53 +60,34 @@ const vehicle3 = {
   metadata: null,
   isCar: true,
 };
-const enigmaTruckBind = `<font size="4"><b>${vehicle1.name}</b></font>
-    <br>Plates number: ${vehicle1.platesNumber}
-    <br>Side number: ${vehicle1.sideNumber}
-    <br>Vehicle color: ${vehicle1.color}
-    <br>Type of vehicle: ${vehicle1.type}
-    <br><b>Vehicle range in km: ${vehicle1.rangeKm}</b>
-    <br><b>Battery level: ${vehicle1.batteryLevelPct}%</b>
-    <br><b>Reservation ends in: ${vehicle1.reservationEnd}</b>
-    <br><b>Vehicle is reserved by: ${vehicle1.reservation}</b>
-    <br><b>Status of car: ${vehicle1.status}</b>
-    <br><b>Location: ${vehicle1.locationDescription}</b>
-    <br>Current promotions: ${vehicle1.promotion}
-    <br>Description of the vehicle: ${vehicle1.description}
-    <br>Metadata of the vehicle: ${vehicle1.metadata}
-    <br>ID of the vehicle: ${vehicle1.id}`;
+const vehicleArray = [vehicle1, vehicle2, vehicle3];
+const details = vehicleArray.map(vehicle => {
+  if (!vehicle.reservationEnd || vehicle.reservation) {
+    vehicle.reservationEnd = 'Not reserved by anyone';
+    vehicle.reservation = 'Not reserved';
 
-const nissanCarBind = `<font size="4"><b>${vehicle2.name}</b></font>
-<br>Plates number: ${vehicle2.platesNumber}
-<br>Side number: ${vehicle2.sideNumber}
-<br>Vehicle color: ${vehicle2.color}
-<br>Type of vehicle: ${vehicle2.type}
-<br><b>Vehicle range in km: ${vehicle2.rangeKm}</b>
-<br><b>Battery level: ${vehicle2.batteryLevelPct}%</b>
-<br><b>Reservation ends in: ${vehicle2.reservationEnd}</b>
-<br><b>Vehicle is reserved by: ${vehicle2.reservation}</b>
-<br><b>Status of car: ${vehicle2.status}</b>
-<br><b>Location: ${vehicle2.locationDescription}</b>
-<br>Current promotions: ${vehicle2.promotion}
-<br>Description of the vehicle: ${vehicle2.description}
-<br>Metadata of the vehicle: ${vehicle2.metadata}
-<br>ID of the vehicle: ${vehicle2.id}`;
-
-const enigmaCarBind = `<font size="4"><b>${vehicle3.name}</b></font>
-<br>Plates number: ${vehicle3.platesNumber}
-<br>Side number: ${vehicle3.sideNumber}
-<br>Vehicle color: ${vehicle3.color}
-<br>Type of vehicle: ${vehicle3.type}
-<br><b>Vehicle range in km: ${vehicle3.rangeKm}</b>
-<br><b>Battery level: ${vehicle3.batteryLevelPct}%</b>
-<br><b>Reservation ends in: ${vehicle3.reservationEnd}</b>
-<br><b>Vehicle is reserved by: ${vehicle3.reservation}</b>
-<br><b>Status of car: ${vehicle3.status}</b>
-<br><b>Location: ${vehicle3.locationDescription}</b>
-<br>Current promotions: ${vehicle3.promotion}
-<br>Description of the vehicle: ${vehicle3.description}
-<br>Metadata of the vehicle: ${vehicle3.metadata}
-<br>ID of the vehicle: ${vehicle3.id}`;
+    if (!vehicle.promotion) vehicle.promotion = 'Excluded from promotion';
+    if (!vehicle.description) vehicle.description = 'No description';
+    if (!vehicle.metadata) vehicle.metadata = 'No metadata';
+  }
+});
+const transformedVehicleArray = vehicleArray.map(
+  vehicle => `<font size="4"><b>${vehicle.name}</b></font>
+<br>Plates number: ${vehicle.platesNumber}
+<br>Side number: ${vehicle.sideNumber}
+<br>Vehicle color: ${vehicle.color}
+<br>Type of vehicle: ${vehicle.type}
+<br><b>Vehicle range in km: ${vehicle.rangeKm}</b>
+<br><b>Battery level: ${vehicle.batteryLevelPct}%</b>
+<br><b>Reservation ends in: ${vehicle.reservationEnd}</b>
+<br><b>Vehicle is reserved by: ${vehicle.reservation}</b>
+<br><b>Status of car: ${vehicle.status}</b>
+<br><b>Location: ${vehicle.locationDescription}</b>
+<br>Current promotions: ${vehicle.promotion}
+<br>Description of the vehicle: ${vehicle.description}
+<br>Metadata of the vehicle: ${vehicle.metadata}
+<br>ID of the vehicle: ${vehicle.id}`
+);
 
 const mymap = L.map('mapid').setView([52.19349790168748, 20.93058149735026], 13);
 
@@ -168,44 +124,97 @@ flyToParking();
 parking1.bindPopup(`<b><font size = "3">Parking for vehicles.</font></b>
 <br>There is located a parking for Our vehicles.`);
 
-//const truckMarker = L.marker([52.1935161702226, 20.9304286193486]).addTo(mymap);
-//const carMarker1 = L.marker([52.1935051400699, 20.930787718473074]).addTo(mymap);
-
 const truckIcon = L.icon({
-  iconUrl: '/assets/a_truck.png',
+  iconUrl: 'assets/a_truck.png',
   iconSize: [35, 35],
   iconAnchor: [20, 25],
   popupAnchor: [-3, -15],
 });
 const carIcon = L.icon({
-  iconUrl: '/assets/a_car.png',
+  iconUrl: 'assets/a_car.png',
   iconSize: [35, 35],
   iconAnchor: [20, 25],
   popupAnchor: [-3, -15],
 });
 
 const addressPoints = [
-  [52.1935161702226, 20.9304286193486, vehicle1.name],
-  [52.193275, 20.930372, vehicle2.name],
-  [52.193891367697, 20.930564789789, vehicle3.name],
+  {
+    lat: 52.1935161702226,
+    lon: 20.9304286193486,
+    vehicle: vehicle1,
+  },
+  {
+    lat: 52.193275,
+    lon: 20.930372,
+    vehicle: vehicle2,
+  },
+  {
+    lat: 52.193891367697,
+    lon: 20.930564789789,
+    vehicle: vehicle3,
+  },
 ];
 
-const marker = [];
 const markers = L.markerClusterGroup();
-for (let i = 0; i < addressPoints.length; i++) {
-  let a = addressPoints[i];
-  let title = a[2];
-  marker[i] = L.marker(new L.LatLng(a[0], a[1]), {
-    title: title,
-    icon: marker[0] ? carIcon : truckIcon,
-  });
-  if (title === `${vehicle1.name}`) {
-    marker[i].bindPopup(enigmaTruckBind);
-  } else if (title === `${vehicle2.name}`) {
-    marker[i].bindPopup(nissanCarBind);
-  } else {
-    marker[i].bindPopup(enigmaCarBind);
+
+function rebuildMap(config) {
+  const marker = [];
+  markers.clearLayers();
+  for (let i = 0; i < addressPoints.length; i++) {
+    let addressPoint = addressPoints[i];
+    if (
+      config.typesPresented.has(addressPoint.vehicle.type) &&
+      (!config.showAvailableOnly || addressPoint.vehicle.status == 'AVAILABLE')
+    ) {
+      let title = addressPoint.vehicle.name;
+      marker[i] = L.marker(new L.LatLng(addressPoint.lat, addressPoint.lon), {
+        title: title,
+        icon: addressPoint.vehicle.type == 'CAR' ? carIcon : truckIcon,
+      });
+      if (title === `${vehicle1.name}`) {
+        marker[i].bindPopup(transformedVehicleArray[0]);
+      } else if (title === `${vehicle2.name}`) {
+        marker[i].bindPopup(transformedVehicleArray[1]);
+      } else {
+        marker[i].bindPopup(transformedVehicleArray[2]);
+      }
+      markers.addLayer(marker[i]);
+    }
   }
-  markers.addLayer(marker[i]);
+  mymap.addLayer(markers);
 }
-mymap.addLayer(markers);
+
+const MAP_FILTERS_CONFIG = {
+  typesPresented: new Set(['CAR', 'TRUCK']),
+  showAvailableOnly: false,
+};
+
+rebuildMap(MAP_FILTERS_CONFIG);
+
+document.getElementById('car-filter').onclick = function () {
+  console.log('Has Car? ' + MAP_FILTERS_CONFIG.typesPresented.has('CAR'));
+  if (MAP_FILTERS_CONFIG.typesPresented.has('TRUCK')) {
+    MAP_FILTERS_CONFIG.typesPresented.delete('TRUCK');
+    rebuildMap(MAP_FILTERS_CONFIG);
+  } else if (!MAP_FILTERS_CONFIG.typesPresented.has('TRUCK')) {
+    MAP_FILTERS_CONFIG.typesPresented.add('TRUCK');
+    rebuildMap(MAP_FILTERS_CONFIG);
+  }
+};
+document.getElementById('truck-filter').onclick = function () {
+  if (MAP_FILTERS_CONFIG.typesPresented.has('CAR')) {
+    MAP_FILTERS_CONFIG.typesPresented.delete('CAR');
+    rebuildMap(MAP_FILTERS_CONFIG);
+  } else {
+    MAP_FILTERS_CONFIG.typesPresented.add('CAR');
+    rebuildMap(MAP_FILTERS_CONFIG);
+  }
+};
+document.getElementById('show-all-filter').onclick = function () {
+  if (MAP_FILTERS_CONFIG.typesPresented.has('CAR', 'TRUCK')) {
+    rebuildMap(MAP_FILTERS_CONFIG);
+  } else {
+    MAP_FILTERS_CONFIG.typesPresented.add('CAR', 'TRUCK');
+    rebuildMap(MAP_FILTERS_CONFIG);
+  }
+};
